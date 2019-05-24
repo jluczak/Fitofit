76dd9ba3feb1c890e69df1f46e25a5259ea8036c
jluczak <jluczaksas@gmail.com>
2019-05-24 17:35:01 +0200

    Add tests for Acivity model + distance validation

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,5 @@
 class Activity < ApplicationRecord
-  validates :start_point, :end_point, presence:true
+  validates :start_point, :end_point, presence: true
+  validates :distance, presence: true , numericality: { only_float: true }
   belongs_to :user
 end
