be3123f602ed98a9652d37baba46196f7bbc2d03
jluczak <jluczaksas@gmail.com>
2019-05-24 17:34:17 +0200

    Add database constraints on activity model

--- db/migrate/20190524143602_create_activities.rb
+++ db/migrate/20190524143602_create_activities.rb
@@ -1,9 +1,9 @@
 class CreateActivities < ActiveRecord::Migration[5.2]
   def change
     create_table :activities do |t|
-      t.string :start_point
-      t.string :end_point
-      t.float :distance
+      t.string :start_point, null: false
+      t.string :end_point, null: false
+      t.float :distance, null: false
       t.references :user, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_143602) do
+ActiveRecord::Schema.define(version: 2019_05_24_141854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "activities", force: :cascade do |t|
-    t.string "start_point"
-    t.string "end_point"
-    t.float "distance"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_activities_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,5 +27,4 @@ ActiveRecord::Schema.define(version: 2019_05_24_143602) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "activities", "users"
 end
