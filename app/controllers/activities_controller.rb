94fcc83c19a59094280cfe523fd3e54fcf41b305
jluczak <jluczaksas@gmail.com>
2019-05-25 09:41:04 +0200

    Generate Activities controller

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,10 @@
+class ActivitiesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'activities/new'
+  get 'activities/create'
+  get 'activities/show'
   devise_for :users
 end
