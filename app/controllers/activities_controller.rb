e207f7fe4f5cee23ae845be17c429e35a7517fa0
jluczak <jluczaksas@gmail.com>
2019-05-25 10:59:40 +0200

    Update Activities controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,10 +1,31 @@
 class ActivitiesController < ApplicationController
   def new
+    @activity = Activity.new
   end
 
   def create
+    @activity = Activity.new(activity_params)
+    @activity.user = current_user
+    @activity.distance = calculate_distance()
+    if @activity.save
+      flash[:success] = "You have just added a new activity!"
+      redirect_to @activity
+    else
+      render 'new'
+    end
   end
 
   def show
   end
+
+  private
+    def calculate_distance
+      start_point_coordinates = Geocoder.coordinates(activity_params[:start_point])
+      end_point_coordinates = Geocoder.coordinates(activity_params[:end_point])
+      (Geocoder::Calculations.distance_between(start_point_coordinates, end_point_coordinates, :units => :km)).round(2)
+    end
+
+    def activity_params
+      params.require(:activity).permit(:start_point, :end_point)
+    end
 end
