ffd81e4731949319c8b9bbe924dd1fc2b9448baa
jluczak <jluczaksas@gmail.com>
2019-05-25 13:59:28 +0200

    Add AddressValidator

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,6 @@
 class Activity < ApplicationRecord
-  validates :start_point, :end_point, presence: true
-  validates :distance, presence: true , numericality: { only_float: true }
+  validates :start_point, :end_point, presence: true, format: { with: /.+\,.+\,.+/i, message: "please enter adress in correct format"}
+  validates :distance, presence: true , numericality: { only_float: true, greater_than: 0 }
+  validates_with AddressValidator
   belongs_to :user
 end
