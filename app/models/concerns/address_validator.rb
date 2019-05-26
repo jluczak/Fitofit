3c6aa936d34b8453a036732be4646b3e7b63d7d0
jluczak <jluczaksas@gmail.com>
2019-05-26 21:24:39 +0200

    Delete unecessary error messages

--- app/models/concerns/address_validator.rb
+++ app/models/concerns/address_validator.rb
@@ -2,11 +2,10 @@
 
 class AddressValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, 'invalid address') if invalid_address?(value)
+    record.errors.add(attribute, 'is invalid') if invalid_address?(value)
     if same_address?(record.start_point, record.end_point)
       message = 'same start point and end point'
-      record.errors.add(:start_point, message)
-      record.errors.add(:end_point, message)
+      record.errors.add(attribute, message)
     end
   end
 
